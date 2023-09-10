9f1dda1e0b4323eb2642b0aec496401462a0a8bd
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-09-10 11:54:24 +0100

    the square properly documented

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+Square class
+"""
+
 
 class square():
+    """Square class model"""
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """initialises the height and width"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +20,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Perimeter of a Square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """strng representation of the square object"""
         return "{}/{}".format(self.width, self.height)
 
 
